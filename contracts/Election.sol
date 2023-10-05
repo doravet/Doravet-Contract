8406fc7d0ca1e4d0b7f73dd5046a83e9f89d84b5
Ultra-tech-code <hizick27@gmail.com>
2023-10-05 18:11:49 +0100

    fixes and rename

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,217 +1,196 @@
-// SPDX-License-Identifier: UNLICENSED
+// // SPDX-License-Identifier:GPL-3.0
 pragma solidity ^0.8.19;
 
-contract Vote {
-    //state Variables
-    address private admin;
-    uint256 private campaignId;
-    address[] private votersList;
-    Campaign[] private allCampaigns;
+// import "@openzeppelin/contracts/utils/Counters.sol";
+// import "hardhat/console.sol";
 
+// contract Election {
+//    using Counters for Counters.Counter;
+//    Counters.Counter public candidateId;
+//    Counters.Counter public votersId;
 
-    //events
-    event Voted(address indexed voter, uint256 indexed campaignId, uint256 indexed time);
-    event CampaignCreated(address indexed campaignCreator, uint256 indexed campaignId, uint256 indexed time);
+//    // Candidate Data
+//    struct Candidate {
+//       uint256 canditateId;
+//       string name;
+//       address candidateAddress;
+//       uint256 voteCount;
+//    }
+//    event LogCandidate(
+//        uint256 canditateId,
+//        string name,
+//        address candidateAddress,
+//        uint256 voteCount
+//    );
+//    address[] public candidateAddresses;
+//    mapping(address => Candidate) public candidates;
 
+//    // Voters Data
+//    struct Voter {
+//        uint256 voterId;
+//        bool voted;
+//        uint256 weight;
+//        uint256 vote;
+//        address voterAddress;
+//        string voterName;
+//    }
+//    event LogVoter (
+//        bool voted,
+//        uint256 weight,
+//        uint256 vote,
+//        address voterAddress
+//    );
 
-    struct Campaign{
-        address campaignCreator;
-        string name;
-        string campaignDescription;
-        uint256 startTime;
-        uint256 duration;
-        uint voteCount;
-        address[] candidate;
-    }
+//    address[] public votedVoters;
+//    address[] public votersAddresses;
+//    mapping(address => Voter) public voters;
 
-    struct Candidate {
-        address candidateAddress;
-        string candidateName;
-        uint256 campaignId;
-        uint256 voteAccumulated;
-        address[] candidateVoters;
-    }
+//    address public chairperson;
+//    uint256 private votingStartTime = block.timestamp;
+//    uint256 private votingEndTime;
 
-    constructor(){
-       admin = msg.sender;
-    }
-    
+//    modifier onlyOwner {
+//        require(msg.sender == chairperson, "You are not the chairperson");
+//        _;
+//    }
 
-    // modifiers
-    modifier onlyAdmin(){
-        require(msg.sender == admin, "NOTADMIN!!");
-        _;
-    }
+//    modifier votingStatus{
+//        require(block.timestamp >= votingStartTime, "Voting Ongoing");
+//        require(block.timestamp <= votingEndTime, "Voting has ended");
+//        _;
+//    }
 
-    modifier onlyVoter(){
-        require(voters[msg.sender] == true, "NOT VOTER!!");
-        _;
-    }
+//    constructor() {
+//        chairperson =  msg.sender;
+//        votingEndTime = votingStartTime + 2 hours;
+//    }
 
-    modifier votersAndAdmin(){
-        require(voters[msg.sender] == true || msg.sender == admin, "NOT VOTER OR ADMIN!!");
-        _;
-    }
+//    function addCandidate(string memory _name, address _candidateAddress) public  onlyOwner votingStatus {
+//        require(candidates[_candidateAddress].candidateAddress == address(0), "candidate already registered");
+//        require(_candidateAddress != chairperson, "Chairperson cannot be a candidate");
 
-    modifier campaignIdcheck(uint256 _campaignId){
-        require(campaigns[_campaignId].campaignCreator != address(0), "Inavlid Campaign Id");
-        _;
-    }
+//        candidateId.increment();
+//        uint256 id = candidateId.current();
+//        Candidate storage candidate = candidates[_candidateAddress];
+//        candidate.canditateId = id;
+//        candidate.candidateAddress = _candidateAddress;
+//        candidate.name = _name;
+//        candidate.voteCount = 0;
+//        candidateAddresses.push(_candidateAddress);
+//        emit LogCandidate(id, _name, _candidateAddress, 0);
+//    }
 
+//    function getCandidates() public view returns (address[] memory){
+//        return candidateAddresses;
+//    }
 
+//    function getCandidateLength() public view returns (uint256){
+//        return candidateAddresses.length;
+//    }
 
-    //mappings
-    mapping(address => bool) private voters;
-    mapping(uint256 => Campaign) private campaigns;
-    mapping(address => mapping(uint256 => Candidate)) candidates;
-    mapping(address => mapping(uint256 => bool)) candidateRegistered;
-    mapping(address => uint256[]) private allUserCampaings;
-    mapping(uint256 => address[]) private allCampaignVoters;
-    mapping(address => mapping(uint256 => bool)) private hasVoted;
+//    function getCandidateData(address _candidateAddress) public view returns(uint256, string memory, address, uint256){
+//        return(
+//            candidates[_candidateAddress].canditateId,
+//            candidates[_candidateAddress].name,
+//            candidates[_candidateAddress].candidateAddress,
+//            candidates[_candidateAddress].voteCount
+//        );
+//    }
 
-    /**
-     * @dev function for admin to register voters
-     */
-    function registerVoter(address _voter) public onlyAdmin{
-        voters[_voter] = true;
-        votersList.push(_voter);
-    }
+//    function giveVoterRight(address _voterAddress) public onlyOwner votingStatus{
+//        votersId.increment();
+//        uint256 id = votersId.current();
+//        Voter storage voter = voters[_voterAddress];
+//        require(voter.weight == 0, "You have already registered");
+//        voter.voterId = id;
+//        voter.weight = 1;
+//        voter.voterAddress = _voterAddress;
+//        voter.voted = false;
+//        votersAddresses.push(_voterAddress);
+//    }
 
-    /**
-     * @dev function for creating campaign
-     */
-    function createCampaign(string memory _CampaingName, string memory _campaignDescription,uint256 _startTime, uint256 _duration) votersAndAdmin public{
-        bytes memory strBytes = bytes(_CampaingName);
-        require(strBytes.length > 0, "Invalid CampaingName");
-        require(_duration > 0, "Invalid duration");
+//    function vote(address _candidateAddress, uint256 _candidate_id) public votingStatus {
+//        Voter storage voter = voters[msg.sender];
+//        require(!voter.voted, "You have already voted");
+//        require(voter.weight != 0,  "You don't have right to vote");
+//        require( _candidateAddress == candidates[_candidateAddress].candidateAddress,  "Candidate does not exist");
 
-        uint256 userCampaignId = campaignId;
-        uint256 campaignDuration = block.timestamp + _duration;
-        uint256 campaignStartTime = block.timestamp + _startTime;
-        address[] memory candidate;
-        campaigns[userCampaignId] = Campaign(msg.sender, _CampaingName,_campaignDescription, campaignStartTime, campaignDuration, 0, candidate);
-        allUserCampaings[msg.sender].push(userCampaignId);
-        campaignId ++;
+//        voter.voted = true;
+//        voter.vote = _candidate_id;
 
-        emit CampaignCreated(msg.sender, userCampaignId, block.timestamp);
-    }
+//        votedVoters.push(msg.sender);
 
-    function registerCandidate(address _candidate, string memory _name, uint256 _campaignId) public campaignIdcheck(_campaignId){
-        require(campaigns[_campaignId].startTime > block.timestamp, "Registration period is over");
-        require(campaigns[_campaignId].campaignCreator == msg.sender, "Not Creator");
-        require(candidateRegistered[_candidate][_campaignId] == false, "Registered Candidate");
-        address[] memory _voters;
-        candidates[_candidate][_campaignId] = Candidate(_candidate, _name, _campaignId, 0, _voters);
-        candidateRegistered[_candidate][_campaignId] = true;
-        campaigns[_campaignId].candidate.push(_candidate);
-    }
-       
-    /**
-     * @dev function for registered voters to vote
-     */
-    function vote(uint256 _campaignId, address _candidate) public onlyVoter campaignIdcheck(_campaignId){
-        require(hasVoted[msg.sender][_campaignId] == false, "ALREADY VOTED!!");
-        require(campaigns[_campaignId].duration > block.timestamp, "Voting period is over");
-        campaigns[_campaignId].voteCount += 1;
-        candidates[_candidate][_campaignId].voteAccumulated +=1;
-        candidates[_candidate][_campaignId].candidateVoters.push(msg.sender);
-        hasVoted[msg.sender][_campaignId] = true;
-        allCampaignVoters[_campaignId].push(msg.sender);
+//        candidates[_candidateAddress].voteCount += voter.weight;
+//         emit LogVoter (true, voter.weight, _candidate_id, msg.sender);
+//    }
 
-        emit Voted(msg.sender, _campaignId, block.timestamp);
-    }
+//    function getVoterLength() public view returns (uint256) {
+//        return votersAddresses.length;
+//    }
 
-    /**
-     * @dev function to change admin
-     */
-    function changeAdmin(address _newAdmin) public onlyAdmin{
-        admin = _newAdmin;
-    }
+//    function getVoterData(address _voterAddress) public view returns (uint256,bool,uint256,uint256,address,string memory) {
+//        return(
+//            voters[_voterAddress].voterId,
+//            voters[_voterAddress].voted,
+//            voters[_voterAddress].weight,
+//            voters[_voterAddress].vote,
+//            voters[_voterAddress].voterAddress,
+//            voters[_voterAddress].voterName
+//        );
+//    }
 
-    /**
-     * @dev function to return all voters
-     */
-    function getVoters() public view votersAndAdmin returns(address[] memory){
-        return votersList;
-    }
-    
-    /**
-     * @dev function to get a campaign
-     */
-    function getCampaign(uint256 _campaignId) public view campaignIdcheck(_campaignId) votersAndAdmin returns(Campaign memory){
-        return (campaigns[_campaignId]);
-    }
+//    function getVotedVoters() public view returns (address [] memory) {
+//        return votedVoters;
+//    }
 
-    /**
-     * @dev function to get all campaigns created by a user
-     */
-    function AllUserCampaigns(address _userAddress) public view votersAndAdmin returns(Campaign[] memory){
-        uint256[] memory allUserCampaignIndex = allUserCampaings[_userAddress];
-        Campaign[] memory userCampaign = new Campaign[](allUserCampaignIndex.length);
-    
-        for (uint256 i = 0; i < allUserCampaignIndex.length; i++) {
-            uint256 campaignIndex = allUserCampaignIndex[i];
-            require(campaignIndex < campaignId, "Invalid campaign index");
-            userCampaign[i] = campaigns[campaignIndex];
-        }
-    
-        return userCampaign; 
-    }
+//    function getVoterList() public view returns (address [] memory) {
+//        return votersAddresses;
+//    }
 
-    /**
-     * @dev function to get all voters of a campaign
-     */
-    function AllCampaignVoters(uint256 _campaignId) public view campaignIdcheck(_campaignId) votersAndAdmin returns(address[] memory){
-        return allCampaignVoters[_campaignId];
-    }
+//     /**
+//     * @dev Gives ending epoch time of voting
+//     * @return _endTime When the voting ends
+//     */
+//    function getVotingEndTime() public view returns (uint256 _endTime) {
+//        return votingEndTime;
+//    }
 
-    /**
-     * @dev function to get all campaigns
-     */
-    function allCampaign() public view votersAndAdmin returns(Campaign[] memory){
-        Campaign[] memory allCampaignList = new Campaign[](campaignId);
+//    /**
+//     * @dev used to update the voting start & end times
+//     * @param _startime Start time that needs to be updated
+//     */
+//    function updateVotingStartTime(uint256 _startime)
+//        public
+//        onlyOwner
+//    {
+//        require(votingStartTime >= _startime);
+//        votingStartTime += _startime;
+//    }
 
-        for (uint256 i = 0; i < campaignId; i++) {
-            allCampaignList[i] = campaigns[i];
-        }
+//    /**
+//     * @dev To extend the end of the voting
+//     * @param _endTime End time that needs to be updated
+//     */
+//    function extendVotingEndTime(uint256 _endTime)
+//        public
+//        onlyOwner
+//    {
+//        // require(block.timestamp >= votingEndTime, "voting has not ended");
+//        votingEndTime += _endTime;
+//    }
 
-        return allCampaignList;
-    }
+//    // Get winner address and voterCount
+//    function getWinner() public onlyOwner view returns (address _winningCandidate, uint256 _voteCount ) {
+//        require(votingEndTime < block.timestamp, "Please wait for voting to end to see the result");
+//        uint voteCount = 0;
+//        address winnerAddress;
+//        for(uint256 i=0; i<candidateAddresses.length; i++) {
+//            if(candidates[candidateAddresses[i]].voteCount > voteCount) {
+//                voteCount = candidates[candidateAddresses[i]].voteCount;
+//                winnerAddress = candidateAddresses[i];
+//            }
+//        }
+//        return (winnerAddress, voteCount);
+//    }
 
-    function allCampaignCandidate(uint256 _campaignId) public view returns(address[] memory){
-        return campaigns[_campaignId].candidate;
-    }
-
-    /**
-     * @dev function to get vote count of a campaign
-     */
-    function campaignTotalVote(uint256 _campaignId) public view campaignIdcheck(_campaignId) votersAndAdmin returns(uint256){
-        return campaigns[_campaignId].voteCount;
-    }
-
-    function getCandidate(uint256 _campaignId, address _candidate) public view campaignIdcheck(_campaignId) returns(Candidate memory){
-        return candidates[_candidate][_campaignId];
-
-    }
-
-    function getCandidateVote(uint256 _campaignId, address _candidate) public view campaignIdcheck(_campaignId) returns(uint256){
-        return candidates[_candidate][_campaignId].voteAccumulated;
-    }
-
-    function getCampaignWinner(uint256 _campaignId) public view returns(address winner, uint256 voteCount){
-        address[] memory allCandidates = allCampaignCandidate(_campaignId);
-        uint256 highestVoteCount = 0;
-        
-        for (uint256 i = 0; i < allCandidates.length; i++) {
-            address candidateAddress = allCandidates[i];
-            uint256 candidateVoteCount = candidates[candidateAddress][_campaignId].voteAccumulated;
-            
-            if (candidateVoteCount > highestVoteCount) {
-                highestVoteCount = candidateVoteCount;
-                winner = candidateAddress;
-            }
-        }
-        
-        voteCount = highestVoteCount;
-    }
-}
+// }
