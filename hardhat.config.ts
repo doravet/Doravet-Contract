9f4d7fa978236aff447e1c7d2af8c910c201a263
Ultra-tech-code <hizick27@gmail.com>
2023-10-07 20:43:25 +0100

    updated hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: {
+    version: '0.8.19',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY as string]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_API_KEY
+  },
+  
 };
 
 export default config;
