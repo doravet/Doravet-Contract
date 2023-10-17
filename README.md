1e9ff99dad353fce5aca706133c188707e167a37
Ultra-tech-code <hizick27@gmail.com>
2023-10-17 08:51:23 +0100

    deployed

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
+### Deployed address on mumbai = 0xf68F6B997D8c51A1622eEFF57AD6A1628c2F60E6
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,8 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [deployer, member1, member2, member3, member4, member5, member6, member7] = await ethers.getSigners();
-
   const Vote = await ethers.deployContract("Vote");
-
+  
   await Vote.waitForDeployment();
 
   console.log(`Vote   deployed to ${Vote.target}` );
